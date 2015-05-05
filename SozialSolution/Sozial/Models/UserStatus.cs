8fdc577e7cf693057f0d3cc7632d31096d1e23c5
Gilli <gisligudmundss@gmail.com>
2015-05-05 13:38:45 +0000

    Visual Studio starter

---
+++ SozialSolution/Sozial/Models/UserStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Sozial.Models
+{
+    public class UserStatus
+    {
+        public int ID { get; set; }
+        public string Status { get; set; }
+        public DateTime DateInsert { get; set; }
+        public string User { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+    }
+}
---
+++ SozialSolution/Sozial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sozial.Startup))]
+namespace Sozial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
